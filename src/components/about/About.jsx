5050371325345d162dabe1ac2837872cac9d4518
Shivangi Kakkar <skakkar@skakkar-l5jgh5.lan>
2022-12-21 16:13:08 -0500

    navbar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,8 @@
- import React from 'react'
- import './about.css'
- 
- 
- const about = () => {
-   return (
-     <div>About</div>
-   )
- }
- 
- export default about
+import React from 'react'
+import './about.css'
+const about = () => {
+  return (
+    <section id='about'>About</section>
+  )
+}
+export default about
